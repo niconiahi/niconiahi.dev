7a289b4c1c2030eb92350d92b79ec93ff7d4d2e2
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-04-15 17:53:31 -0300

    fix: center text on index projects page

--- app/routes/find/projects/index.tsx
+++ app/routes/find/projects/index.tsx
@@ -1,5 +1,9 @@
 import { ReactElement } from "react"
 
 export default function Index(): ReactElement {
-  return <span className="text-gray-500">Pick a project from the list</span>
+  return (
+    <section className="flex flex-1 items-center">
+      <span className="text-gray-500">Pick a project from the list</span>
+    </section>
+  )
 }
