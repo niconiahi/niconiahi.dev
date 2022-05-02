e71b7bdd29a2f8c6acb147770d133709a07a17b4
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-05-01 22:01:11 -0300

    chore: correct gitignored files

--- apps/web/public/build/routes/find/bio/index-DITRC5VM.js
+++
@@ -1,1 +0,0 @@
-import{h as e,i as t}from"/build/_shared/chunk-DOJI7SJ7.js";t();t();function o(){return e.createElement("h1",null,"What do you want to know about me?")}export{o as default};
--- apps/web/public/build/routes/index-ZAFQDRRF.js
+++
@@ -1,1 +0,0 @@
-import{b as p,i as e}from"/build/_shared/chunk-DOJI7SJ7.js";var r=p((t,o)=>{e();o.exports={}});export default r();
