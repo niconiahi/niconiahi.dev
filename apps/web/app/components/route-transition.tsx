03ee8f18d65ba2388da49f4cfce7f6ec428cc740
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-05-18 20:58:28 -0300

    refactor: move on handler to live on transaction provider

--- apps/web/app/components/route-transition.tsx
+++ apps/web/app/components/route-transition.tsx
@@ -1,15 +1,23 @@
-import { useTransition } from "@remix-run/react"
 import type { ReactElement } from "react"
+import { Transition } from "@headlessui/react"
+import { useTransition } from "@remix-run/react"
 
-export default function RouteTransition(): ReactElement | null {
+export default function RouteTransition(): ReactElement {
   const transition = useTransition()
 
-  if (transition.state === "loading")
-    return (
+  return (
+    <Transition
+      enter="transition-opacity duration-150"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-150"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+      show={transition.state === "loading"}
+    >
       <div className="fixed bottom-4 right-4 bg-gray-700 text-gray-50 p-2 rounded-md">
         <p>Loading route</p>
       </div>
-    )
-
-  return null
+    </Transition>
+  )
 }
