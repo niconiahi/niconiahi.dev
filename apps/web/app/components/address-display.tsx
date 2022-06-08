ccb4cf70aeaf6bf8fe127d4d133790df8c77a94c
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-06-08 20:42:36 -0300

    feat: add db as package + configure vitest + first smoke tests

--- apps/web/app/components/address-display.tsx
+++ apps/web/app/components/address-display.tsx
@@ -1,6 +1,6 @@
 import type { ReactElement } from "react"
 
-import { truncateString } from "~/helpers"
+import { truncate } from "~/helpers"
 
 export default function AddressDisplay({
   account,
@@ -11,7 +11,7 @@ export default function AddressDisplay({
     <header className="flex w-full justify-end">
       <aside className="border-2 p-2 border-gray-900 rounded-md flex space-x-2 items-center justify-center">
         <span className="w-2 h-2 bg-green-400 rounded outline outline-1 outline-gray-900" />
-        <h1>{truncateString(account)}</h1>
+        <h1>{truncate(account)}</h1>
       </aside>
     </header>
   )
---
+++ packages/testing/vitest.config.js
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./helpers/setup.ts"],
+    include: ["./app/**/*.{test}.{ts,tsx}"],
+    watchIgnore: [".*\\/node_modules\\/.*", ".*\\/build\\/.*"],
+  },
+})
