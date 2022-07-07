4eedd5a70e040dfd7465a736eb7b68f109fe7b39
Nicolas Accetta <nicolas.accetta@gmail.com>
2022-07-07 18:20:21 -0300

    feat: improve route transition animation

--- apps/web/app/components/route-transition.tsx
+++ apps/web/app/components/route-transition.tsx
@@ -1,19 +1,13 @@
 import type { ReactElement } from "react"
-import { Transition } from "@headlessui/react"
 import { useTransition } from "@remix-run/react"
 
-export default function RouteTransition(): ReactElement {
+export default function RouteTransition(): ReactElement | null {
   const transition = useTransition()
 
-  return (
-    <Transition
-      enter="transition-all duration-15000"
-      enterFrom="w-0"
-      enterTo="w-screen"
-      show={transition.state === "loading"}
-      role="progressbar"
-    >
-      <div className="fixed top-0 left-0 bg-gray-700 w-full h-2" />
-    </Transition>
-  )
+  if (transition.state === "loading")
+    return (
+      <div className="fixed top-0 left-0 bg-gray-700 h-1.5 route-transition w-full" />
+    )
+
+  return null
 }
