27d7ac1b71e19a6d42dd67e9dcef96e37b8d7009
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-10-09 12:49:30 -0300

    feat: add initial article migration

---
+++ db/migrations/20231009154105073_add_article.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "users" (
+  "slug" TEXT NOT NULL PRIMARY KEY,
+  "hash" TEXT NOT NULL,
+  "title" TEXT NOT NULL,
+  "description" TEXT NOT NULL
+  "html" TEXT NOT NULL,
+  "createdAt" DATETIME DEFAULT CURRENT_TIMESTAMP, 
+  "updatedAt" DATETIME
+);
