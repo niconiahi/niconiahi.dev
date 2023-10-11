a0be75946e51beace55b1444100728f4af5b0b34
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-10-11 14:21:45 -0300

    fix: update site url constant

--- app/utils/routes.ts
+++ app/utils/routes.ts
@@ -1,5 +1,5 @@
-// export const NICONIAHI_DEV_URL = "https://niconiahi-dev.pages.dev";
-export const NICONIAHI_DEV_URL = "http://localhost:8788";
+export const NICONIAHI_DEV_URL = "https://niconiahi-dev.pages.dev";
+// export const NICONIAHI_DEV_URL = "http://localhost:8788";
 export const ROUTES = {
   getArticles() {
     return `/article/get/all`;
