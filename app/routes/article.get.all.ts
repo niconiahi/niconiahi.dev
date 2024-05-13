225fc5c5fd1c0c23be3d0c4801bda95de7837ffb
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-05-13 10:40:54 -0300

    chore: remove logs

--- app/routes/article.get.all.ts
+++ app/routes/article.get.all.ts
@@ -3,12 +3,9 @@ import { getQueryBuilder } from "~/utils/query-builder.server"
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const queryBuilder = getQueryBuilder(context)
-  console.log('queryBuilder', queryBuilder)
   const articles = await queryBuilder
     .selectFrom("article")
     .select(["slug", "title"])
     .execute()
-  console.log('articles', articles)
-
   return json(articles)
 }
--- app/utils/routes.ts
+++ app/utils/routes.ts
@@ -1,12 +1,10 @@
 import type { Env } from "~/utils/env.server"
 
 export const PRODUCTION_ORIGIN = "https://niconiahi.pages.dev"
-export const DEVELOPMENT_ORIGIN = "http://localhost:8788"
+export const DEVELOPMENT_ORIGIN = "http://localhost:5173"
 
 export function getOrigin(env: Env) {
-  console.log('env', env)
-  return PRODUCTION_ORIGIN
-  // return env.ENVIRONMENT === "development"
-  //   ? DEVELOPMENT_ORIGIN
-  //   : PRODUCTION_ORIGIN
+  return env.ENVIRONMENT === "development"
+    ? DEVELOPMENT_ORIGIN
+    : PRODUCTION_ORIGIN
 }
