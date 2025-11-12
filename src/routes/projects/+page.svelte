c92b92eb3c38b801fd4b92d8867a0e241b930c3f
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-11-12 15:17:42 -0300

    feat: add other projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
   import Title from "$lib/components/title.svelte";
-  import Card from "./components/card.svelte";
+  import Card, { type Project } from "./components/card.svelte";
   import ethernauta_img from "$lib/assets/ethernauta.png";
   import peer_to_peer_videoconference_img from "$lib/assets/peer-to-peer-videoconference.png";
   import ariabones_img from "$lib/assets/ariabones.png";
   import morfar_img from "$lib/assets/morfar.png";
-  type Project = {
-    title: string;
-    description: string;
-    date: Date;
-    image: string;
-  };
+  import animatronik_img from "$lib/assets/animatronik.png";
+  import olga_img from "$lib/assets/olga.png";
+  import cryptoman_img from "$lib/assets/cryptoman.png";
+
   const projects: Project[] = [
     {
       title: "Etherenauta",
@@ -21,7 +19,7 @@
     },
     {
       title: "Morfar",
-      date: new Date(2025, 7, 20),
+      date: new Date(2025, 4, 11),
       image: morfar_img,
       description:
         `Restaurant management application which helps owners set dietary restrictions for their products automatically
@@ -29,16 +27,41 @@
 On the other end, users get completely reliable dietary restrictions's filters when navigating the restaurant's menu`.trim(),
     },
     {
-      title: "Peer-to-peer videoconference application",
+      title: "Olga",
+      date: new Date(2024, 4, 5),
+      image: olga_img,
+      description:
+        `This application extracts all chapters for every uploaded video for every show of a streaming channel called Olga
+
+It then lists these chapters with the corresponding link that navigates to the expected video in the expected time`.trim(),
+    },
+    {
+      title: "Cryptoman",
+      date: new Date(2025, 8, 7),
+      image: cryptoman_img,
+      description: `An Ethereum wallet written completely from scratch
+
+It uses the base building blocks provided by my other project, Ethernauta`.trim(),
+    },
+    {
+      title: "Peerference",
       date: new Date(2024, 5, 9),
       image: peer_to_peer_videoconference_img,
       description:
-        `Google Meet alternative which allows you to connect call another peorson without signing up`.trim(),
+        `Google Meet alternative which allows you to call another person without signing up`.trim(),
+    },
+    {
+      title: "Animatronik",
+      date: new Date(2024, 4, 18),
+      image: ariabones_img,
+      description: `Share your CSS collectible with the world
+
+The foundational technology used is blockchain NFTs`.trim(),
     },
     {
       title: "Ariabones",
-      date: new Date(2024, 5, 9),
-      image: ariabones_img,
+      date: new Date(2024, 1, 19),
+      image: animatronik_img,
       description:
         `Modular and atomic unstyled web components that you can easily install and use
 
@@ -61,5 +84,6 @@ You take care of the styles, the library takes care of the accessibility`.trim()
     grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
     grid-auto-rows: masonry;
     gap: 1rem;
+    padding-bottom: 4rem;
   }
 </style>
--- src/routes/projects/components/card.svelte
+++ src/routes/projects/components/card.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  type Project = {
+  export type Project = {
     title: string;
     description: string;
     date: Date;
@@ -16,10 +16,7 @@
     }),
   );
 
-  let tilt_x = $state(0);
-  let tilt_y = $state(0);
-  const max_tilt = 10;
-
+  const MAX_TILT = 10;
   function handle_mouse_move(event: MouseEvent) {
     const target = event.currentTarget as HTMLElement;
     const rect = target.getBoundingClientRect();
@@ -27,15 +24,13 @@
     const y = (event.clientY - rect.top) / rect.height;
     const normalized_x = x * 2 - 1;
     const normalized_y = y * 2 - 1;
-    tilt_x = -normalized_y * max_tilt;
-    tilt_y = normalized_x * max_tilt;
+    const tilt_x = -normalized_y * MAX_TILT;
+    const tilt_y = normalized_x * MAX_TILT;
     target.style.transform = `perspective(800px) rotateX(${tilt_x}deg) rotateY(${tilt_y}deg)`;
   }
 
   function handle_mouse_leave(event: MouseEvent) {
     const target = event.currentTarget as HTMLElement;
-    tilt_x = 0;
-    tilt_y = 0;
     target.style.transform = `perspective(800px) rotateX(0deg) rotateY(0deg)`;
   }
 </script>
@@ -73,11 +68,12 @@
   img {
     box-shadow: 2px 2px 6px var(--neutral-400);
     border-radius: 0.25rem;
+    aspect-ratio: 1;
   }
   span {
     color: var(--neutral-600);
     font-size: 0.75rem;
-    font-weight: bold;
+    font-weight: 500;
   }
   p {
     white-space: pre-line;
